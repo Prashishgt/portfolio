50824898200af52cb7d732e6a272f538e3727433
Prashishgt <prashish24@gmail.com>
2024-05-03 07:03:27 +0545

    Removed email js utils due to it causing build error

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,34 +1,34 @@
-import emailjs, { EmailJSResponseStatus } from "emailjs-com";
+// import emailjs, { EmailJSResponseStatus } from "emailjs-com";
 
-interface EmailParams {
-  to_email: string;
-  subject: string;
-  message: string;
-}
+// interface EmailParams {
+//   to_email: string;
+//   subject: string;
+//   message: string;
+// }
 
-export const sendEmail = async (
-  recipientEmail: string,
-  subject: string,
-  message: string
-): Promise<EmailJSResponseStatus> => {
-  try {
-    const params: EmailParams = {
-      to_email: recipientEmail,
-      subject: subject,
-      message: message,
-    };
+// export const sendEmail = async (
+//   recipientEmail: string,
+//   subject: string,
+//   message: string
+// ): Promise<EmailJSResponseStatus> => {
+//   try {
+//     const params: EmailParams = {
+//       to_email: recipientEmail,
+//       subject: subject,
+//       message: message,
+//     };
 
-    const response = await emailjs.send(
-      `${process.env.NEXT_PUBLIC_EMAILJS_URL}`,
-      "",
-      params,
-      `${process.env.NEXT_PUBLIC_EMAILJS_USERID}`
-    );
+//     const response = await emailjs.send(
+//       `${process.env.NEXT_PUBLIC_EMAILJS_URL}`,
+//       "",
+//       params,
+//       `${process.env.NEXT_PUBLIC_EMAILJS_USERID}`
+//     );
 
-    console.log("Email sent successfully:", response);
-    return response;
-  } catch (error) {
-    console.error("Error sending email:", error);
-    throw error;
-  }
-};
+//     console.log("Email sent successfully:", response);
+//     return response;
+//   } catch (error) {
+//     console.error("Error sending email:", error);
+//     throw error;
+//   }
+// };
